8f54af94ee64a8b727e3dd43a56e89a6496d94b2
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-15 19:45:26 +0300

    person class created

---
+++ app/src/main/java/com/gusoft/mvvm_roomdb_example/db/person.kt
@@ -0,0 +1,17 @@
+package com.gusoft.mvvm_roomdb_example.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "person_table")
+data class person(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "person_name")
+    val name: String,
+    @ColumnInfo(name = "person_surname")
+    val surname: String,
+    @ColumnInfo(name = "phone_number")
+    val phone: String
+)
