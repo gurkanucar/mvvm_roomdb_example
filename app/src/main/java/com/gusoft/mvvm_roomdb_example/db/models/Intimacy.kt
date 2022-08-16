e32cb8fb844d5fa405358711ec18587ee9aecb40
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-16 12:47:17 +0300

    intimacy model added to person

---
+++ app/src/main/java/com/gusoft/mvvm_roomdb_example/db/models/Intimacy.kt
@@ -0,0 +1,5 @@
+package com.gusoft.mvvm_roomdb_example.db.models
+
+enum class Intimacy {
+    FAMILY, FRIEND, JOB, OTHER
+}
--- app/src/main/java/com/gusoft/mvvm_roomdb_example/db/models/Person.kt
+++ app/src/main/java/com/gusoft/mvvm_roomdb_example/db/models/Person.kt
@@ -1,10 +1,13 @@
-package com.gusoft.mvvm_roomdb_example.db
+package com.gusoft.mvvm_roomdb_example.db.models
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "person_table")
+@Parcelize
 data class Person(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
@@ -13,5 +16,6 @@ data class Person(
     @ColumnInfo(name = "person_surname")
     val surname: String,
     @ColumnInfo(name = "phone_number")
-    val phone: String
-)
+    val phone: String,
+    val intimacy: Intimacy
+) : Parcelable
